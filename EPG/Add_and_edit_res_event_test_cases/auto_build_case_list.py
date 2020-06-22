588a7d999d67097b47f13b2cdad0b13f7996dd26
wangrun002 <506667882@qq.com>
2020-06-22 20:27:59 +0800

    将所有编辑组合的情况的list补全

--- EPG/Add_and_edit_res_event_test_cases/auto_build_case_list.py
+++ EPG/Add_and_edit_res_event_test_cases/auto_build_case_list.py
@@ -61,6 +61,151 @@ for x in res_type:
                 single_list[9] = x
                 total_list.append(single_list)
                 n += 1
+# ModifyTime+ModifyType
+for x in res_mode:
+    for y in res_type:
+        for z in res_type:
+            if z != y:
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "ModifyTime+ModifyType", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = x
+                single_list[4] = y
+                single_list[8] = x
+                single_list[9] = z
+                total_list.append(single_list)
+                n += 1
+# ModifyTime+ModifyDuration
+for x in res_mode:
+    for y in res_type:
+        if y == "PVR":
+            single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                           "ModifyTime+ModifyDuration", "", "", "screen_test_numb"]
+            single_list[0] = "{0:02d}".format(n)
+            single_list[3] = x
+            single_list[4] = y
+            single_list[8] = x
+            single_list[9] = y
+            total_list.append(single_list)
+            n += 1
+# ModifyTime+ModifyMode
+for x in res_type:
+    for y in res_mode:
+        for z in res_mode:
+            if z != y:
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "ModifyTime+ModifyMode", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = y
+                single_list[4] = x
+                single_list[8] = z
+                single_list[9] = x
+                total_list.append(single_list)
+                n += 1
+# ModifyType+ModifyDuration
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            if j != "PVR" and k == "PVR":
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "ModifyType+ModifyDuration", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = j
+                single_list[4] = h
+                single_list[8] = k
+                single_list[9] = h
+                total_list.append(single_list)
+                n += 1
+# ModifyType+ModifyMode
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            for i in res_mode:
+                if k != j and i != h:
+                    single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                                   "ModifyType+ModifyMode", "", "", "screen_test_numb"]
+                    single_list[0] = "{0:02d}".format(n)
+                    single_list[3] = j
+                    single_list[4] = h
+                    single_list[8] = k
+                    single_list[9] = i
+                    total_list.append(single_list)
+                    n += 1
+# ModifyDuration+ModifyMode
+for x in res_type:
+    for y in res_mode:
+        for z in res_mode:
+            if x == "PVR" and z != y:
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "ModifyDuration+ModifyMode", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = y
+                single_list[4] = x
+                single_list[8] = z
+                single_list[9] = x
+                total_list.append(single_list)
+                n += 1
+# ModifyTime+ModifyType+ModifyDuration
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            if j != "PVR" and k == "PVR":
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "ModifyTime+ModifyType+ModifyDuration", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = h
+                single_list[4] = j
+                single_list[8] = h
+                single_list[9] = k
+                total_list.append(single_list)
+                n += 1
+# ModifyTime+ModifyType+ModifyMode
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            for i in res_mode:
+                if k != j and i != h:
+                    single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                                   "ModifyTime+ModifyType+ModifyMode", "", "", "screen_test_numb"]
+                    single_list[0] = "{0:02d}".format(n)
+                    single_list[3] = h
+                    single_list[4] = j
+                    single_list[8] = i
+                    single_list[9] = k
+                    total_list.append(single_list)
+                    n += 1
+# ModifyType+ModifyDuration+ModifyMode
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            for i in res_mode:
+                if j != "PVR" and k == "PVR":
+                    if k != j and i != h:
+                        single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                                       "ModifyType+ModifyDuration+ModifyMode", "", "", "screen_test_numb"]
+                        single_list[0] = "{0:02d}".format(n)
+                        single_list[3] = h
+                        single_list[4] = j
+                        single_list[8] = i
+                        single_list[9] = k
+                        total_list.append(single_list)
+                        n += 1
+# ModifyTime+ModifyType+ModifyDuration+ModifyMode
+for j in res_type:
+    for k in res_type:
+        for h in res_mode:
+            for i in res_mode:
+                if j != "PVR" and k == "PVR":
+                    if k != j and i != h:
+                        single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                                       "ModifyTime+ModifyType+ModifyDuration+ModifyMode", "", "", "screen_test_numb"]
+                        single_list[0] = "{0:02d}".format(n)
+                        single_list[3] = h
+                        single_list[4] = j
+                        single_list[8] = i
+                        single_list[9] = k
+                        total_list.append(single_list)
+                        n += 1
 
 # print(len(total_list))
 for m in range(len(total_list)):
