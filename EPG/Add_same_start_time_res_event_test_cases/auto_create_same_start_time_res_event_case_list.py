45466e163b10d4f6095fe6a3397b1c4cfbf8eba3
wangrun002 <506667882@qq.com>
2020-07-21 16:10:56 +0800

    增加新增相同起始时间事件的test case自动生成的脚本

---
+++ EPG/Add_same_start_time_res_event_test_cases/auto_create_same_start_time_res_event_case_list.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+screen_test_numb = 20
+other_interface_test_numb = 50
+total_list = []
+n = 0
+res_type = ["Play", "PVR", "Power Off", "Power On"]
+res_mode = ["Once", "Daily", "Weekly"]
+scenes_list = [
+    "Same(time+type+mode)",
+    "Same(time+mode)+Diff(type)",
+    "Same(time+type)+Diff(mode)",
+    "Same(time)+Diff(type+mode)",
+    ]
+# "Same(time+type+mode)"
+for i in res_type:
+    for j in res_mode:
+        single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                       "Same[time+type+mode]", "", "", "screen_test_numb"]
+        single_list[0] = "{0:02d}".format(n)
+        single_list[3] = j
+        single_list[4] = i
+        single_list[8] = j
+        single_list[9] = i
+        total_list.append(single_list)
+        n += 1
+
+# "Same(time+mode)+Diff(type)"
+for i in res_mode:
+    for j in res_type:
+        for k in res_type:
+            if k != j:
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "Same[time+mode]+Diff[type]", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = i
+                single_list[4] = j
+                single_list[8] = i
+                single_list[9] = k
+                total_list.append(single_list)
+                n += 1
+
+# "Same(time+type)+Diff(mode)"
+for i in res_type:
+    for j in res_mode:
+        for k in res_mode:
+            if k != j:
+                single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                               "Same[time+type]+Diff[mode]", "", "", "screen_test_numb"]
+                single_list[0] = "{0:02d}".format(n)
+                single_list[3] = j
+                single_list[4] = i
+                single_list[8] = k
+                single_list[9] = i
+                total_list.append(single_list)
+                n += 1
+
+# "Same(time)+Diff(type+mode)"
+for i in res_type:
+    for j in res_type:
+        for k in res_mode:
+            for h in res_mode:
+                if j != i and h != k:
+                    single_list = ["", "All", "TV", "", "", "TVScreenDiffCH", "Manual_jump",
+                                   "Same[time]+Diff[type+mode]", "", "", "screen_test_numb"]
+                    single_list[0] = "{0:02d}".format(n)
+                    single_list[3] = k
+                    single_list[4] = i
+                    single_list[8] = h
+                    single_list[9] = j
+                    total_list.append(single_list)
+                    n += 1
+
+# print(len(total_list))
+for m in range(len(total_list)):
+    print(f"{total_list[m]},")
