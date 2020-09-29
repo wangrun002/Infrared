044a1adad904300ec613d48115cb5bf4418d009f
wangrun002 <506667882@qq.com>
2020-09-29 17:41:35 +0800

    重新组合修改case

--- EPG/Invalid_res_event_test_cases/auto_create_invalid_res_event_case_list.py
+++ EPG/Invalid_res_event_test_cases/auto_create_invalid_res_event_case_list.py
@@ -40,32 +40,105 @@ for j in epg_res_scenes:
             n += 1
 
 # Timer Setting界面
+#
 # ["01", "GX", "TV", "Play", "Once", "OutOfSaveTimeRange", "Timer", "Boundary_before_upper_limit", "ZeroTimezone", "epg_test_numb"]
-for i in time_zone:
-    for j in timer_res_type:
-        for k in out_of_range:
-            # if i == "ZeroTimezone":
-            single_list = ["", "GX", "TV", "", "Once", "OutOfSaveTimeRange", "Timer", "", "", "epg_test_numb"]
-            single_list[0] = "{0:02d}".format(n)
-            single_list[3] = j
-            single_list[7] = k
-            single_list[8] = i
-            total_list.append(single_list)
-            n += 1
+# for i in time_zone:
+#     for j in timer_res_type:
+#         for k in out_of_range:
+#             # if i == "ZeroTimezone":
+#             single_list = ["", "GX", "TV", "", "Once", "OutOfSaveTimeRange", "Timer", "", "", "epg_test_numb"]
+#             single_list[0] = "{0:02d}".format(n)
+#             single_list[3] = j
+#             single_list[7] = k
+#             single_list[8] = i
+#             total_list.append(single_list)
+#             n += 1
+
+# out of range + play + ZeroTimezone + NoSummertime
+for k in out_of_range:
+    single_list = ["", "GX", "TV", "Play", "Once", "OutOfSaveTimeRange", "Timer", "",
+                   "ZeroTimezone", "NoSummertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+
+# out of range + PVR + ZeroTimezone + Summertime
+for k in out_of_range:
+    single_list = ["", "GX", "TV", "PVR", "Once", "OutOfSaveTimeRange", "Timer", "",
+                   "ZeroTimezone", "Summertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+
+# out of range + Power Off + OtherTimezone + NoSummertime
+for k in out_of_range:
+    single_list = ["", "GX", "TV", "Power Off", "Once", "OutOfSaveTimeRange", "Timer", "",
+                   "OtherTimezone", "NoSummertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+# out of range + Power On + OtherTimezone + Summertime
+for k in out_of_range:
+    single_list = ["", "GX", "TV", "Power On", "Once", "OutOfSaveTimeRange", "Timer", "",
+                   "OtherTimezone", "Summertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+
 
 # Timer setting Expired用例
-for i in timer_res_type:
-    for j in timer_expired_scenes:
-        single_list = ["", "GX", "TV", "", "Once", "Expired", "Timer", "", "ZeroTimezone", "epg_test_numb"]
-        single_list[0] = "{0:02d}".format(n)
-        single_list[3] = i
-        single_list[7] = j
-        total_list.append(single_list)
-        n += 1
+# for i in timer_res_type:
+#     for j in timer_expired_scenes:
+#         single_list = ["", "GX", "TV", "", "Once", "Expired", "Timer", "", "ZeroTimezone", "epg_test_numb"]
+#         single_list[0] = "{0:02d}".format(n)
+#         single_list[3] = i
+#         single_list[7] = j
+#         total_list.append(single_list)
+#         n += 1
+
+# Expired + play + ZeroTimezone + NoSummertime
+for k in timer_expired_scenes:
+    single_list = ["", "GX", "TV", "Play", "Once", "Expired", "Timer", "",
+                   "ZeroTimezone", "NoSummertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+
+# Expired + PVR + ZeroTimezone + Summertime
+for k in timer_expired_scenes:
+    single_list = ["", "GX", "TV", "PVR", "Once", "Expired", "Timer", "",
+                   "ZeroTimezone", "Summertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+
+# Expired + Power Off + OtherTimezone + NoSummertime
+for k in timer_expired_scenes:
+    single_list = ["", "GX", "TV", "Power Off", "Once", "Expired", "Timer", "",
+                   "OtherTimezone", "NoSummertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
+# Expired + Power On + OtherTimezone + Summertime
+for k in timer_expired_scenes:
+    single_list = ["", "GX", "TV", "Power On", "Once", "Expired", "Timer", "",
+                   "OtherTimezone", "Summertime", "epg_test_numb"]
+    single_list[0] = "{0:02d}".format(n)
+    single_list[7] = k
+    total_list.append(single_list)
+    n += 1
 
 # Timer setting NowPlaying用例
 for i in timer_res_type:
-    single_list = ["", "GX", "TV", "", "Once", "NowPlaying", "Timer", "None", "ZeroTimezone", "epg_test_numb"]
+    single_list = ["", "GX", "TV", "", "Once", "NowPlaying", "Timer", "None",
+                   "ZeroTimezone", "NoSummertime", "epg_test_numb"]
     single_list[0] = "{0:02d}".format(n)
     single_list[3] = i
     total_list.append(single_list)
@@ -75,7 +148,8 @@ for i in timer_res_type:
 for i in timer_res_type:
     for j in timer_res_mode:
         if i == "PVR":
-            single_list = ["", "GX", "TV", "", "", "InvalidDuration", "Timer", "None", "ZeroTimezone", "epg_test_numb"]
+            single_list = ["", "GX", "TV", "", "", "InvalidDuration", "Timer", "None",
+                           "ZeroTimezone", "NoSummertime", "epg_test_numb"]
             single_list[0] = "{0:02d}".format(n)
             single_list[3] = i
             single_list[4] = j
